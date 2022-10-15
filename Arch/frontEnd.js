7369a1363059d43a3a3f617f8302237995790880
sagethewizard <jag6248@psu.edu>
2022-10-15 10:47:04 -0400

    Boosted some Boot Strap dark theme, added server info and zomboid place holder

--- Arch/frontEnd.js
+++ Arch/frontEnd.js
@@ -4,6 +4,7 @@ const express = require('express');
 
 const DOM1_IP = '192.168.1.12';
 const INDEX_PATH = "./webgui/index.html"
+const ZAMZAM_INDEX_PATH = "./webgui/index.html"
 
 const PORT = 8888;
 const app = express();
@@ -12,6 +13,12 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
     webpageURL = fs.readFileSync(INDEX_PATH)
+    res.status(200)
+    res.contentType("text/html")
+    res.send(webpageURL)
+});
+app.get('/zomboid/', function (req, res) {
+    webpageURL = fs.readFileSync(ZAMZAM_INDEX_PATH)
     res.status(200)
     res.contentType("text/html")
     res.send(webpageURL)
