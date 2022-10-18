330abeee61f7fbd1518e90f6fe73bc9f837ce310
sagethewizard <jag6248@psu.edu>
2022-10-18 19:29:03 -0400

    Adding Jarvis Heart Beat first Attempt

--- Arch/frontEndWeb/frontEnd.js
+++ Arch/frontEndWeb/frontEnd.js
@@ -9,8 +9,12 @@ const ZAMZAM_INDEX_PATH = "./webgui/zomboid/index.html"
 const PORT = 8888;
 const app = express();
 
+var jarvisInfo = {
+    "uptime": 0
+}
 app.use(bodyParser.json());
 
+// REST API --- START
 app.get('/', function (req, res) {
     webpageURL = fs.readFileSync(INDEX_PATH)
     res.status(200)
@@ -25,11 +29,25 @@ app.get('/zomboid/', function (req, res) {
 });
 
 app.post('/rest/1/jarvis/', function (req, res) {
-
+    jarvisInfo["uptime"] = req.body["uptime"]
 })
+
+app.get('/rest/1/jarvis/', function (req, res) {
+    res.status(200)
+    res.contentType("application/json")
+    res.send(JSON.stringify(jarvisInfo))
+})
+// REST API ---- END
 app.listen(PORT, DOM1_IP, (err) => {
     if (err) {
         throw err;
     }
     console.log('Node Endpoints working :)');
 });
+
+function msToString(ms) {
+    var d, h, m, s
+    s = Math.floor(ms / 1000)
+    m = Math.floor(s / 60)
+
+}
